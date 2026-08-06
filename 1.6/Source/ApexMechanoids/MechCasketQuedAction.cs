13b09bcd848fb09fef8a9b3a44f4b21387f8d733
Veltaris <mw_wiest@web.de>
2026-08-06 21:37:25 +0200

    Qued action system (#79)
    - added qued actions
    - simplified command gizmo on mechs
    - added progress bar when repairing

---
+++ 1.6/Source/ApexMechanoids/MechCasketQuedAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace ApexMechanoids
+{
+    public class MechCasketQuedAction
+    {
+
+        public LocalTargetInfo targetinfo = null;
+
+        public int action = 0;
+
+
+        public MechCasketQuedAction(LocalTargetInfo targetinfo, int action)
+        {
+            this.targetinfo = targetinfo;
+            this.action = action;
+        }
+
+    }
+}
