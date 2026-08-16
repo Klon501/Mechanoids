cc970ee38521ea001ad07f8c87cca6957d32dfda
VeItaris <mw_wiest@web.de>
2026-08-16 18:31:02 +0200

    Command Casket for scenario
    - added ancient command casket for scenario
    - fixed amplifier size
    - added/updated descriptions for javelin, command casket & amplifier
    - added texture for ejecting mechanitor from casket

---
+++ 1.6/Source/ApexMechanoids/Buildings/Building_AncientCommandCasket.cs
@@ -0,0 +1,85 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ApexMechanoids
+{
+    public class Building_AncientCommandCasket : Building_Casket
+    {
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            FillBuilding();
+        }
+
+        public override void PostMake()
+        {
+            base.PostPostMake();
+            FillBuilding();
+            if (HitPoints != def.BaseMaxHitPoints)  // this is needed for resons I don't understand. Spawns with -1 hitpoint otherwise
+            {
+                HitPoints = def.BaseMaxHitPoints;
+            }
+        }
+
+        public override bool Accepts(Thing thing)
+        {
+            if (!base.Accepts(thing))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void FillBuilding()
+        {
+            if (!HasAnyContents)
+            {
+                PawnGenerationRequest request = new PawnGenerationRequest(PawnKindDefOf.Mechanitor_Basic, Find.FactionManager.OfAncientsHostile,
+                    forceGenerateNewPawn: true,
+                    allowDead: true, allowDowned: false, canGeneratePawnRelations: false, allowPregnant: false, allowFood: false, allowAddictions: true, certainlyBeenInCryptosleep: true, forceNoIdeo: false, forceNoBackstory: false,
+                    forbidAnyTitle: false, forceDead: true,
+                    developmentalStages: DevelopmentalStage.Adult
+                    );
+                Pawn pawnToSpawn = PawnGenerator.GeneratePawn(request);
+
+                innerContainer.TryAddOrTransfer(pawnToSpawn.Corpse);
+            }
+        }
+
+        public override void Open()
+        {
+            if (HasAnyContents)
+            {
+                EjectContents();
+                if (!openedSignal.NullOrEmpty())
+                {
+                    Find.SignalManager.SendSignal(new Signal(openedSignal, this.Named("SUBJECT")));
+                }
+                DirtyMapMesh(base.Map);
+                Utils.ReplaceBuilding(this, ApexDefsOf.APM_MechCommandCasket);
+            }
+        }
+
+
+        public override void DeSpawn(DestroyMode mode = DestroyMode.Vanish)
+        {
+            if (mode != DestroyMode.WillReplace)
+            {
+                EjectContents();
+            }
+            base.DeSpawn(mode);
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+        }
+
+   
+
+
+
+
+    }
+}
